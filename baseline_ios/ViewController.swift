5f9c6279e386cdee389df4dffc0edd6e1f432565
Harvin Llanes <harvin.llanes@gmail.com>
2018-12-05 18:32:11 -0600

    Initial Commit

---
+++ baseline_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  baseline_ios
+//
+//  Created by Harvin Llanes on 12/5/18.
+//  Copyright © 2018 Harvin Llanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ baseline_iosTests/baseline_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  baseline_iosTests.swift
+//  baseline_iosTests
+//
+//  Created by Harvin Llanes on 12/5/18.
+//  Copyright © 2018 Harvin Llanes. All rights reserved.
+//
+
+import XCTest
+@testable import baseline_ios
+
+class baseline_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
